3fd0d87fe8623a8fd5644c7b427a570f0f1c6265
guanchishan <guanchishan@gmail.com>
2023-06-18 16:25:15 +0900

    edit intro

--- json2csv/outbox_json2csv_one_id_one_line_change_timezone.py
+++ json2csv/outbox_json2csv_one_id_one_line_change_timezone.py
@@ -8,7 +8,7 @@ file_path = "output.csv"
 # 读取 CSV 文件
 df = pd.read_csv(file_path)
 
-# 转换时间
+# 转换时间。(hours=9)即给待处理的时间戳加9个钟头。可修改为自己所需要的
 df['published'] = pd.to_datetime(df['published']) + timedelta(hours=9)
 
 # 将 DataFrame 转换为 CSV 文件
